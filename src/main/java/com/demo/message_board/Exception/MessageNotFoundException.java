e9171155751626a6d5b5a648ed255872f82c2f58
Aswad Abbas <aswad.abbas@2iqresearch.com>
2022-08-23 14:42:14 +0500

    Customs Exceptions Created

---
+++ src/main/java/com/demo/message_board/Exception/MessageNotFoundException.java
@@ -0,0 +1,25 @@
+package com.demo.message_board.Exception;
+
+public class MessageNotFoundException extends RuntimeException{
+    public MessageNotFoundException() {
+        super();
+    }
+
+    public MessageNotFoundException(String message) {
+        super(message);
+    }
+
+    public MessageNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    @Override
+    public String getMessage() {
+        return "Requested Message is not found";
+    }
+
+    @Override
+    public String getLocalizedMessage() {
+        return "Requested Message is not found";
+    }
+}
---
+++ src/main/java/com/demo/message_board/Exception/UnAuthorizedMessageAccess.java
@@ -0,0 +1,30 @@
+package com.demo.message_board.Exception;
+
+public class UnAuthorizedMessageAccess extends RuntimeException {
+    public UnAuthorizedMessageAccess() {
+        super();
+    }
+
+    public UnAuthorizedMessageAccess(String message) {
+        super(message);
+    }
+
+    public UnAuthorizedMessageAccess(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    @Override
+    public String getMessage() {
+        return "User Not allowed to access message.";
+    }
+
+    @Override
+    public String getLocalizedMessage() {
+        return  "User Not allowed to access message.";
+    }
+
+    @Override
+    public synchronized Throwable getCause() {
+        return super.getCause();
+    }
+}
