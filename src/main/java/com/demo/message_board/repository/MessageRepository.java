e2ce9588d80bda6b626158591bbdae42b81696b8
Aswad Abbas <aswad.abbas@2iqresearch.com>
2022-08-24 01:17:23 +0500

    Repository Updated

--- src/main/java/com/demo/message_board/repository/MessageRepository.java
+++ src/main/java/com/demo/message_board/repository/MessageRepository.java
@@ -1,10 +1,10 @@
 package com.demo.message_board.repository;
 
 import com.demo.message_board.entities.Message;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageRepository extends CrudRepository<Message, Integer> {
+public interface MessageRepository extends JpaRepository<Message, Integer> {
 
 }
