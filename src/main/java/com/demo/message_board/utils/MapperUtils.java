77809f2931c7fbbde1b432f5bc6051edc6452daa
Aswad Abbas <aswad.abbas@2iqresearch.com>
2022-08-24 09:12:52 +0500

    Get Mapper added to Mapper Utils

--- src/main/java/com/demo/message_board/utils/MapperUtils.java
+++ src/main/java/com/demo/message_board/utils/MapperUtils.java
@@ -1,6 +1,12 @@
 package com.demo.message_board.utils;
 
 
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
 public class MapperUtils {
 
+    public static Mapper getMapper(Class<Mapper> tClass) {
+        return Mappers.getMapper(tClass);
+    }
 }
