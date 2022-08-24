3570aa667227d6966e14d1bda4d273f3d069efce
Aswad Abbas <aswad.abbas@2iqresearch.com>
2022-08-24 11:34:46 +0500

    Removed Users from Message DTO

--- src/main/java/com/demo/message_board/dto/MessageDTO.java
+++ src/main/java/com/demo/message_board/dto/MessageDTO.java
@@ -1,14 +1,15 @@
 package com.demo.message_board.dto;
 
-import com.demo.message_board.entities.User;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Builder
 public class MessageDTO {
     private int id;
     private String content;
-    private UserDTO owner;
 }
